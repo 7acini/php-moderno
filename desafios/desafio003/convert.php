93a9de3d904913369bf362641502abf7881a6ce4
7acini <guilherme.facini@protonmail.com>
2025-02-14 17:19:24 -0300

    :refactor: style(desafio003): Melhorando formatação do valor monetário adicionando 2 casas decimais

--- desafios/desafio003/convert.php
+++ desafios/desafio003/convert.php
@@ -10,12 +10,12 @@
     <section>
         <h1>Conversor de Moedas v1.0</h1>
         <?php 
-        const DOLAR = 5.77;
+        const COTACAO = 5.77;
         $moneyReais = (float) $_GET["money"];
-        $moneyDolar = $moneyReais / DOLAR;
+        $moneyDolar = $moneyReais / COTACAO;
 
-        echo "Seus R$ $moneyReais equivalem a US$ <strong>$moneyDolar</strong> <br>";
-        echo "Cotação fixa de R$ " . DOLAR . " informada diretamente no código <br>" ;
+        echo "Seus R$ ".number_format($moneyReais, 2)." equivalem a US$ <strong>".number_format($moneyDolar, 2)."</strong> <br>";
+        echo "Cotação fixa de R$ " . number_format(COTACAO, 2) . " informada diretamente no código <br>" ;
         ?>
         <input type="button" value="Voltar" onclick="history.go(-1)">
     </section>
