9347cdb27ab655a3105ba16569e2e95297585bd5
7acini <guilherme.facini@protonmail.com>
2025-02-16 12:15:37 -0300

    :sparkles: feat(ex005): Finalizando exercicio sobre variaveis superglobais

--- exercicios/ex005/superglobais.php
+++ exercicios/ex005/superglobais.php
@@ -8,18 +8,38 @@
 </head>
 <body>
     <main>
-        <?php 
-        echo "<h1>Superglobal GET</h1>";
-        var_dump($_GET);
+        <pre>
+            <?php 
+            echo "<h1>Superglobal GET</h1>";
+            var_dump($_GET);
 
-        echo "<h1>Superglobal POST</h1>";
-        var_dump($_POST);
+            echo "<h1>Superglobal POST</h1>";
+            var_dump($_POST);
 
-        echo "<h1>Superglobal REQUEST</h1>";
-        var_dump($_REQUEST);
+            echo "<h1>Superglobal REQUEST</h1>";
+            var_dump($_REQUEST);
 
-        
-        ?>
+            echo "<h1>Superglobal SERVER</h1>";
+            var_dump($_SERVER);
+
+            echo "<h1>Superglobal FILES</h1>";
+            var_dump($_FILES);
+
+            echo "<h1>Superglobal ENV</h1>";
+            var_dump($_ENV["PATH"]);
+
+            echo "<h1>Superglobal SESSION</h1>";
+            var_dump($_SESSION);
+
+            echo "<h1>Superglobal COOKIE</h1>";
+            var_dump($_COOKIE);
+
+            echo "<h1>Superglobal GLOBALS</h1>";
+            var_dump($GLOBALS);
+
+
+            ?>            
+        </pre>
     </main>
 </body>
 </html>
