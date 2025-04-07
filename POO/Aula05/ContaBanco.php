9a7e36fbcd33ecb72772f60911db9bcc8cbf2a9f
7acini <guilherme.facini@protonmail.com>
2025-04-07 17:22:23 -0300

    :technologist: feat(aula05): Realizado exercicio antes de ver a próxima aula, sujeito a alterações

---
+++ POO/Aula05/ContaBanco.php
@@ -0,0 +1,106 @@
+<?php
+
+class ContaBanco{
+    public int $numConta;
+    protected string $tipo;
+    private string $dono;
+    private float $saldo;
+    private bool $status;
+
+    public function __construct(){
+        $this->saldo = 0;
+        $this->status = false;
+    }
+
+    public function getNumConta(): int{
+        return $this->numConta;
+    }
+    public function setNumConta(int $numConta){
+        $this->numConta = $numConta;
+    }
+    public function getTipo(): string{
+        return $this->tipo;
+    }
+    public function setTipo(string $tipo){
+        $this->tipo = $tipo;
+    }
+    public function getDono(): string{
+        return $this->dono;
+    }
+    public function setDono(string $dono){
+        $this->dono = $dono;
+    }
+    public function getSaldo(): float{
+        return $this->saldo;
+    }
+    public function setSaldo(float $saldo){
+        $this->saldo = $saldo;
+    }
+    public function getStatus(): bool{
+        return $this->status;
+    }
+    public function setStatus(bool $status){
+        $this->status = $status;
+    }
+
+    public function abrirConta(string $tipo){
+        $this->setTipo($tipo);
+        $this->setStatus(true);
+        if ($tipo == "CC"){
+            $this->setSaldo(50);
+        }
+        elseif($tipo == "CP"){
+            $this->setSaldo(150);
+        }
+    }
+
+    public function fecharConta(){
+        if($this->getSaldo() >= 0){
+            echo "Conta com dinheiro";
+        }
+        elseif($this->getSaldo() < 0) {
+            echo "Conta em débito";
+        }
+        else {
+            $this->setStatus(false);
+        }
+    }
+
+    public function depositar(float $valor) {
+        if($this->getStatus() == true) {
+            $this->setSaldo($this->getSaldo() + $valor);
+        }
+        else{
+            echo "Impossível depositar";
+        }
+    }
+    public function sacar(float $valor) {
+        if($this->getStatus() == true) {
+            $this->setSaldo($this->getSaldo() - $valor);
+        }
+        else{
+            echo "Impossível sacar";
+        }
+    }
+
+    public function pagarMensal(){
+        if($this->getTipo() == "CC") {
+            $valor = 12;
+        }
+        elseif($this->getTipo() == "CP") {
+            $valor = 20;
+        }
+        if($this->getStatus() == true) {
+            if($this->getSaldo() > $valor) {
+                $this->setSaldo($this->getSaldo() - $valor);
+            }
+            else {
+                echo "Saldo insuficiente";
+            }
+        }
+        else {
+            echo "Impossivel pagar";
+        }
+    }
+
+}
